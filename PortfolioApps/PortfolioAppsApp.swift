aa6b3cfd4af20a87854f6ff291c0864070d77895
Sumit Gera <sumit.gera@grabtaxi.com>
2024-01-15 18:40:35 +0530

    Initial Commit

---
+++ PortfolioApps/PortfolioAppsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortfolioAppsApp.swift
+//  PortfolioApps
+//
+//  Created by Sumit Gera on 15/01/24.
+//
+
+import SwiftUI
+
+@main
+struct PortfolioAppsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
